b7f02801e912e5d117214ded818a79f61d9c471a
suneel <suneel.sahu@kimshospitals.com>
2025-04-17 23:47:05 +0530

    .

--- components/Work.jsx
+++ components/Work.jsx
@@ -31,7 +31,7 @@ const Work = ({isDarkMode}) => {
       whileInView={{ opacity: 1 }}
       transition={{ delay: 0.7, duration: 0.5 }}
       className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
-      Welcome to my web development portfolio! Explore a collection of projects showcasing my expertise in front-end development.</motion.p>
+      Explore a collection of projects showcasing my expertise in web and mobile app development.</motion.p>
 
     <motion.div
     initial={{ opacity: 0 }}
@@ -66,7 +66,7 @@ const Work = ({isDarkMode}) => {
     initial={{ opacity: 0 }}
     whileInView={{ opacity: 1 }}
     transition={{ delay: 1.1, duration: 0.5 }}
-    href="" className='w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500 dark:text-white dark:border-white dark:hover:bg-darkHover'>
+    href="https://github.com/sahusuneel777/" target='blank' className='w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500 dark:text-white dark:border-white dark:hover:bg-darkHover'>
         Show more 
         <Image src={isDarkMode ? assets.right_arrow_bold_dark : assets.right_arrow_bold} alt='Right arrow' className='w-4'/>
     </motion.a>
